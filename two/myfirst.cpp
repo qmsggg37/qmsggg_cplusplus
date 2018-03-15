6dfdc127994db303c1380d7b2139a9f2bea03337
252717 <hank199025@gmail.com>
2018-03-15 14:19:38 +0800

    重读C++primerplus

---
+++ two/myfirst.cpp
@@ -0,0 +1,12 @@
+// myfirst.cpp--displays a message
+
+#include <iostream>
+int main() 
+{
+    using namespace std;
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+
+    cout << "You won`t regret it!" << endl;
+    return 0;
+}
