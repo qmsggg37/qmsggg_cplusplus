5e3ba535b40cef296af518e5c72b45b1a2b9c0bd
qmsggg <luohao0414@gmail.com>
2018-12-02 16:18:54 +0800

    array base iter

---
+++ code/cpp_standard_library/array/array_base_test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <array>
+#include <string>
+
+using namespace std;
+
+// constexpr string DEFAULT_NAME("DEFAULT_NAME");
+
+int main(int argc, char const *argv[])
+{
+    // array<char, 41> a = {DEFAULT_NAME};
+
+    /* strcpy(&a[0], "hello, world");
+       printf("%s\n", &a[0]);
+       printf("%s\n", a.data());
+     */
+    return 0;
+}
